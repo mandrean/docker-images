03bb1aca7048c09f631b6cc971e8d508612a4f8e
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2017-09-24 03:28:20 +0200

    shrink-go: create image (#1)

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Pre-requisites
 --------------
 * [Docker](https://docs.docker.com/engine/installation/)
 
+Images
+------
+| Name                            | Description                                                                                           |
+|---------------------------------|-------------------------------------------------------------------------------------------------------|
+| [mandrean/shrink-go](shrink-go) | Build image for compiling & shrinking statically linked Go binaries into their smallest possible size |
+
+
 Maintainers
 -----------
 * Sebastian Mandrean (<sebastian.mandrean@gmail.com>)
---
+++ shrink-go/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+PROJ=$1
+
+printf "\nInitiating shrink-go for $PROJ... "
+cd $PROJ
+if [ $? -ne 0 ]; then
+    echo -e "ERROR: Couldn't find $GOPATH/src/$PROJ"
+    echo -e "Aborting."
+    exit 1
+fi
+printf "Done.\n"
+
+printf "Building statically linked binary with stripped symbols... "
+go build -a -installsuffix cgo -ldflags="-s -w"
+if [ $? -ne 0 ]; then
+    echo -e "ERROR: Couldn't build $PROJ"
+    echo -e "Aborting."
+    exit 1
+fi
+printf "Done.\n"
+
+
+printf "Packing with upx...\n"
+DIR=$(basename "$PWD")
+upx -f --best --ultra-brute -o $DIR-upx $DIR
+if [ $? -ne 0 ]; then
+    echo -e "ERROR: Couldn't pack $PROJ with upx"
+    echo -e "Aborting."
+    exit 1
+fi
+
+echo "Successfully shrinked $PROJ!"
+echo "Done."
